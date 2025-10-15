94fe859ef0e1ed118a3bace3e27175ba5722b77e
Isaac <>
2025-10-14 23:40:45 -0600

    Replace chars in blaster_tier_1.txt, add code.html

--- vbScript.js
+++ vbScript.js
@@ -17,7 +17,7 @@ navHTML += '<a class="navbar-brand" href="' + relativePath + '/index.html">'+ si
 navHTML += '<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">                <span class="navbar-toggler-icon"></span>            </button>            <div class="collapse navbar-collapse" id="navbarNav">';
 
 // Add Top link
-navHTML += '<ul class="navbar-nav"><li class="nav-item"><a class="nav-link active" aria-current="page" href="' + relativePath + '/index.html">Home</a></li>';
+navHTML += '<ul class="navbar-nav"><li class="nav-item"><a class="nav-link" aria-current="page" href="' + relativePath + '/index.html">Home</a></li>';
 navHTML += '<li class="nav-item"><a class="nav-link" href="' + relativePath + '/english.html">English Work</a></li>';
 navHTML += '<li class="nav-item"><a class="nav-link" href="' + relativePath + '/code.html">Programming Work</a></li>';
 
@@ -27,9 +27,9 @@ navHTML += '</ul></div></div></nav>';
 // HTML INSERT //////////////////////////////////////////
 
 // Insert navbar
-if (document.getElementById("navDiv")) {
-    document.getElementById("navDiv").innerHTML = navHTML;
-}
+// if (document.getElementById("navDiv")) {
+//     document.getElementById("navDiv").innerHTML = navHTML;
+// }
 
 // Dynamically set the <title> tag
 if (document.title === "Site Title") {
